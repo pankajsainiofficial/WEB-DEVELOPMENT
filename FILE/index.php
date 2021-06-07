fe3c4cbd9253912937b4abf896b423b92931ffcb
pankaj <pankajsaini19967@gmail.com>
2021-06-07 19:15:19 +0530

    Co-authored-by: parishjainofficial@gmail.com <parishjainofficial@gmail.com>

--- FILE/index.php
+++ FILE/index.php
@@ -1,3 +1,27 @@
-<?php
-    echo "this is only testing from parish&pankaj`"
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is another affiliate website</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div id="homepage">
+        <div id="header">
+            
+        </div>
+
+
+
+
+
+
+
+
+        
+    </div>
+</body>
+</html>
