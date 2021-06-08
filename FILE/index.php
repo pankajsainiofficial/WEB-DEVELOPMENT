8f4df437435495c99562dfeda5875a27c757c1ff
pankaj <pankajsaini19967@gmail.com>
2021-06-08 17:52:39 +0530

    Co-authored-by: parishjainofficial@gmail.com <parishjainofficial@gmail.com>

--- FILE/index.php
+++ FILE/index.php
@@ -1,17 +1,82 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>This is another affiliate website</title>
     <link rel="stylesheet" href="/css/style.css">
+    <script src="/JS/index.js"></script>
 </head>
+
 <body>
     <div id="homepage">
         <div id="header">
-            
+            <div class="logo">
+                <img src="/images/logo.png" alt="ok">
+                <h3>Lorem ipsum.</h3>
+            </div>
+            <div class="searchbar">
+                <div class="category">
+                    <select name="search">
+                        <option value="Searchin">Search In</option>
+                        <option value="Mobiles">Mobiles</option>
+                        <option value="Laptops">Laptops</option>
+                        <option value="Tablets">Tablets</option>
+                        <option value="Gadgets">Gadgets</option>
+                        <option value="Gaming">Gaming</option>
+                        <option value="Computers">Computers</option>
+                        <option value="Cameras">Cameras</option>
+                        <option value="Electronics">Electronics</option>
+                        <option value="Smart-Home">Smart Home</option>
+                    </select>
+                </div>
+                <div id="searchsubmit">
+                    <input type="text" class="search" placeholder="Search products">
+                </div>
+                <button id="submit" type="submit"></button>
+            </div>
+            <div class="login">
+                <h3>LOGIN</h3>
+            </div>
+            <div class="signup">
+                <h3>SIGNUP</h3>
+            </div>
+            <div class="cart">
+
+
+            </div>
+
+        </div>
+        <!-- -----------------------------------Navbar----------------------------------- -->
+        <div id="navbar">
+            <div id="categories">
+                <ul class="categories_list">
+                    <li>Shop By Categories
+                        <div class="sub-menu">
+                            <ul>
+                                <li>laptop</li>
+                                <li>mobile</li>
+                                <li>mobile</li>
+                                <li>mobile</li>
+                                <li>mobile</li>
+                                <li>mobile</li>
+                            </ul>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+            <div id="nav_list" class="nav-fix">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Shop</li>
+                    <li>Contact Us</li>
+                    <li>Faq</li>
+                </ul>
+            </div>
         </div>
 
 
@@ -21,7 +86,9 @@
 
 
 
-        
+
+
     </div>
 </body>
+
 </html>
