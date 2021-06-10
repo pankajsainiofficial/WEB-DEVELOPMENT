6cf13b20256c3dbbe1d8852d3ab471bf86b22461
pankaj <pankajsaini19967@gmail.com>
2021-06-10 18:56:27 +0530

    Co-authored-by: parishjainofficial@gmail.com <parishjainofficial@gmail.com>

--- FILE/index.php
+++ FILE/index.php
@@ -8,6 +8,9 @@
     <title>This is another affiliate website</title>
     <link rel="stylesheet" href="/css/style.css">
     <script src="/JS/index.js"></script>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 
 <body>
@@ -32,16 +35,20 @@
                         <option value="Smart-Home">Smart Home</option>
                     </select>
                 </div>
-                <div id="searchsubmit">
-                    <input type="text" class="search" placeholder="Search products">
-                </div>
-                <button id="submit" type="submit"></button>
+                <!-- <div id="searchsubmit"> -->
+                <input type="text" class="search" placeholder="Search products">
+                <button id="submit" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
             </div>
             <div class="login">
                 <h3>LOGIN</h3>
             </div>
             <div class="signup">
                 <h3>SIGNUP</h3>
+            </div>
+            <div id="notification">
+                <i class="far fa-bell"></i>
             </div>
             <div class="cart">
 
@@ -52,7 +59,7 @@
         <!-- -----------------------------------Navbar----------------------------------- -->
         <div id="navbar">
             <div id="categories">
-                <ul class="categories_list">
+                <ul class="categories_list nav-fix">
                     <li>Shop By Categories
                         <div class="sub-menu">
                             <ul>
@@ -73,12 +80,68 @@
                     <li>Home</li>
                     <li>About</li>
                     <li>Shop</li>
+                    <li>Top Deals</li>
                     <li>Contact Us</li>
                     <li>Faq</li>
                 </ul>
             </div>
         </div>
 
+        <!-- -----------------------------Carousel------------------------------------ -->
+        <div id="container">
+            <div id="Carousel">
+                <div class="wrapper">
+                    <div class="item-info item-info-fix">
+                        <h1>ipad Pro</h1>
+                        <p>Ipad pro max comes with thullu specification <br> and bund fadu price</p>
+                    </div>
+                    <div class="images">
+                        <div class="slider item-info-fix">
+                            <img src="/images/tech3.jpg" alt="This is first Carousel image ">
+                        </div>
+                        <div class="slider">
+                            <img src="/images/tech1.jpg" alt="This is second Carousel image ">
+                        </div>
+                    </div>
+                </div>
+                <div class="contain">
+                    <img src="/images/tech2.jpg" alt="unable to connect">
+                </div>
+            </div>
+            <!-- ---------------------------------top-category----------------------------->
+            <div id="top-category-heading">
+                <div class="heading">
+                    <h2>Top Categories Of This Month</h2>
+                </div>
+                <div id="top-category">
+                    <div class="Smartphone product ">
+                        <i class="fas fa-mobile-android-alt iconfix"></i>
+                    </div>
+                    <div class="Laptops product">
+                        <span class="material-icons iconfix">
+                            laptop
+                        </span> 
+                    </div>
+                    <div class="Computers product">
+                        <i class="far fa-computer-speaker iconfix"></i>
+                    </div>
+                    <div class="Gadgets product">
+                        <span class="material-icons iconfix">
+                            devices_other
+                        </span>
+
+                    </div>
+                    <div class="Cameras product">
+                        <span class="material-icons iconfix">
+                            photo_camera
+                        </span>
+                    </div>
+                    <div class="Smart-TV product">
+                        <i class="fas fa-tv iconfix"></i>
+                    </div>
+
+                </div>
+            </div>
 
 
 
@@ -88,6 +151,11 @@
 
 
 
+
+
+
+
+        </div>
     </div>
 </body>
 
