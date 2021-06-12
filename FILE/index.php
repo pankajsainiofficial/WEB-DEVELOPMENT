dd65e54955fcb94dcb73cbacc8e07a9c012e17d6
pankaj <pankajsaini19967@gmail.com>
2021-06-12 21:50:57 +0530

    Co-authored-by: parishjainofficial@gmail.com <parishjainofficial@gmail.com>

--- FILE/index.php
+++ FILE/index.php
@@ -50,11 +50,6 @@
             <div id="notification">
                 <i class="far fa-bell"></i>
             </div>
-            <div class="cart">
-
-
-            </div>
-
         </div>
         <!-- -----------------------------------Navbar----------------------------------- -->
         <div id="navbar">
@@ -91,22 +86,52 @@
         <div id="container">
             <div id="Carousel">
                 <div class="wrapper">
-                    <div class="item-info item-info-fix">
-                        <h1>ipad Pro</h1>
-                        <p>Ipad pro max comes with thullu specification <br> and bund fadu price</p>
-                    </div>
-                    <div class="images">
-                        <div class="slider item-info-fix">
+                    <div class="images item-info-fix">
+                        <div class="slider ">
                             <img src="/images/tech3.jpg" alt="This is first Carousel image ">
                         </div>
                         <div class="slider">
                             <img src="/images/tech1.jpg" alt="This is second Carousel image ">
                         </div>
+                        <div class="item-info item-info-fix">
+                            <h1>ipad Pro</h1>
+                            <p>Ipad pro max comes with thullu specification <br> and bekar price</p>
+                        </div>
                     </div>
                 </div>
                 <div class="contain">
-                    <img src="/images/tech2.jpg" alt="unable to connect">
+                    <div class="compare-section">
+                        <h2>Compare Products</h2>
+                        <div class="compare-head">
+                            <div class="main-compare">
+                                <select name="comparing">
+                                    <option value="Mobiles">Mobiles</option>
+                                    <option value="Laptops">Laptops</option>
+                                    <option value="Tablets">Tablets</option>
+                                    <option value="Gadgets">Gadgets</option>
+                                    <option value="Gaming">Gaming</option>
+                                    <option value="Computers">Computers</option>
+                                    <option value="Cameras">Cameras</option>
+                                    <option value="Electronics">Electronics</option>
+                                    <option value="Smart-Home">Smart Home</option>
+                                </select>
+                            </div>
+                            <div class="product-list">
+                                <input type="text" name="product_1" id="product_1" placeholder="Type the firs product">
+                            </div>
+                            <span>
+                                <h4>V/S</h4>
+                            </span>
+                            <div class="product-list">
+                                <input type="text" name="product_2" id="product_2"
+                                    placeholder="Type the second product">
+                            </div>
+                            <button type="submit">Compare Now</button>
+                        </div>
+                    </div>
                 </div>
+
+
             </div>
             <!-- ---------------------------------top-category----------------------------->
             <div id="top-category-heading">
@@ -115,31 +140,67 @@
                 </div>
                 <div id="top-category">
                     <div class="Smartphone product ">
-                        <i class="fas fa-mobile-android-alt iconfix"></i>
+                        <i class="fas fa-mobile-android-alt iconfix color-fix"></i>
+                        <h3 class="color-fix">Smartphone</h3>
                     </div>
-                    <div class="Laptops product">
-                        <span class="material-icons iconfix">
+                    <div class="Laptops-computer product">
+                        <span class="material-icons iconfix color-fix">
                             laptop
-                        </span> 
+                        </span>
+                        <h3 class="color-fix">Computer & Laptops</h3>
                     </div>
-                    <div class="Computers product">
-                        <i class="far fa-computer-speaker iconfix"></i>
+                    <div class="computer-accessories product">
+                        <span class="material-icons iconfix color-fix">
+                            headset
+                        </span>
+                        <h3 class="color-fix">Computer Accessories</h3>
+
                     </div>
                     <div class="Gadgets product">
-                        <span class="material-icons iconfix">
+                        <span class="material-icons iconfix color-fix">
                             devices_other
                         </span>
-
+                        <h3 class="color-fix">Gadgets</h3>
                     </div>
                     <div class="Cameras product">
-                        <span class="material-icons iconfix">
+                        <span class="material-icons iconfix color-fix">
                             photo_camera
                         </span>
+                        <h3 class="color-fix">Cameras</h3>
                     </div>
                     <div class="Smart-TV product">
-                        <i class="fas fa-tv iconfix"></i>
+                        <i class="fas fa-tv iconfix color-fix"></i>
+                        <h3 class="color-fix">TV</h 3>
                     </div>
-
+                    <div class="Home-appliances product">
+                        <span class="material-icons iconfix color-fix">
+                            coffee_maker
+                        </span>
+                        <h3 class="color-fix">Home Appliances</h3>
+                    </div>
+                    <div class="Game&Toy product">
+                        <span class="material-icons iconfix color-fix">
+                            smart_toy
+                        </span>
+                        <h3 class="color-fix">Game & Toy</h3>
+                    </div>
+                </div>
+            </div>
+            <!-- ------------------------arrivals------------------------- -->
+            <div id="arrivals">
+                <div class="arrivals-new">
+                    <div class="mega-discount discount">
+                        <h2> Mega Discount</h2>
+                        <!-- <img src="/images/oneplus.jpg" alt="lets pray to god"> -->
+                    </div>
+                    <div class="weekend-sale discount">
+                        <h2> Weekend Sale</h2>
+                        <!-- <img src="/images/laptop.jpg" alt="lets pray to god"> -->
+                    </div>
+                    <div class="new-arrivals discount">
+                        <h2> New Arrivals </h2>
+                        <!-- <img src="/images/gadgets.jpg" alt="lets pray to god"> -->
+                    </div>
                 </div>
             </div>
 
