b23a338152983e272e734a08d0b5611ad1cce6c8
pankaj <pankajsaini19967@gmail.com>
2021-06-24 22:18:16 +0530

    header splited here

---
+++ FILE/header.php
@@ -0,0 +1,69 @@
+<div id="header">
+            <div class="logo">
+                <img src="/photos/weblogo.png" alt="ok">
+                <h3>Price Pair</h3>
+            </div>
+            <div class="searchbar">
+                <div class="category">
+                    <select name="search">
+                        <option value="Searchin">Search In</option>
+                        <option value="Mobiles">Mobiles</option>
+                        <option value="Laptops">Laptops</option>
+                        <option value="Tablets">Tablets</option>
+                        <option value="Gadgets">Gadgets</option>
+                        <option value="Gaming">Gaming</option>
+                        <option value="Computers">Computers</option>
+                        <option value="Cameras">Cameras</option>
+                        <option value="Electronics">Electronics</option>
+                        <option value="Smart-Home">Smart Home</option>
+                    </select>
+                </div>
+                <!-- <div id="searchsubmit"> -->
+                <input type="text" class="search" placeholder="Search products">
+                <button id="submit" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+            <div class="login">
+                <h3>LOGIN</h3>
+            </div>
+            <div class="signup">
+                <h3>SIGNUP</h3>
+            </div>
+            <div id="notification">
+                <i class="far fa-bell"></i>
+            </div>
+        </div>
+    
+        <!-- -----------------------------------Navbar----------------------------------- -->
+        <div id="navbar">
+            <div id="categories">
+                <ul class="categories_list nav-fix">
+                    <li>Shop By Categories
+                    <i class="fas fa-caret-down"></i>
+                        <div class="sub-menu">
+                            <ul>
+                                <li>laptop</li>
+                                <li>mobile</li>
+                                <li>mobile</li>
+                                <li>mobile</li>
+                                <li>mobile</li>
+                                <li>mobile</li>
+                            </ul>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+            <div id="nav_list" class="nav-fix">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Shop</li>
+                    <li>Top Deals</li>
+                    <li>Contact Us</li>
+                    <li>Faq</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
--- FILE/index.php
+++ FILE/index.php
@@ -8,6 +8,7 @@
     <title>This is another affiliate website</title>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/footer.css">
+    <link rel="stylesheet" href="/css/header.css">
     <script src="/JS/index.js"></script>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
@@ -16,75 +17,9 @@
 
 <body>
     <div id="homepage">
-        <div id="header">
-            <div class="logo">
-                <img src="/photos/weblogo.png" alt="ok">
-                <h3>Price Pair</h3>
-            </div>
-            <div class="searchbar">
-                <div class="category">
-                    <select name="search">
-                        <option value="Searchin">Search In</option>
-                        <option value="Mobiles">Mobiles</option>
-                        <option value="Laptops">Laptops</option>
-                        <option value="Tablets">Tablets</option>
-                        <option value="Gadgets">Gadgets</option>
-                        <option value="Gaming">Gaming</option>
-                        <option value="Computers">Computers</option>
-                        <option value="Cameras">Cameras</option>
-                        <option value="Electronics">Electronics</option>
-                        <option value="Smart-Home">Smart Home</option>
-                    </select>
-                </div>
-                <!-- <div id="searchsubmit"> -->
-                <input type="text" class="search" placeholder="Search products">
-                <button id="submit" type="submit">
-                    <i class="fas fa-search"></i>
-                </button>
-            </div>
-            <div class="login">
-                <h3>LOGIN</h3>
-            </div>
-            <div class="signup">
-                <h3>SIGNUP</h3>
-            </div>
-            <div id="notification">
-                <i class="far fa-bell"></i>
-            </div>
-        </div>
-    
-        <!-- -----------------------------------Navbar----------------------------------- -->
-        <div id="navbar">
-            <div id="categories">
-                <ul class="categories_list nav-fix">
-                    <li>Shop By Categories
-                    <i class="fas fa-caret-down"></i>
-                        <div class="sub-menu">
-                            <ul>
-                                <li>laptop</li>
-                                <li>mobile</li>
-                                <li>mobile</li>
-                                <li>mobile</li>
-                                <li>mobile</li>
-                                <li>mobile</li>
-                            </ul>
-                        </div>
-                    </li>
-
-                </ul>
-            </div>
-            <div id="nav_list" class="nav-fix">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Shop</li>
-                    <li>Top Deals</li>
-                    <li>Contact Us</li>
-                    <li>Faq</li>
-                </ul>
-            </div>
-        </div>
-        <hr>
+        <?php
+            require "header.php";
+        ?>
         <!-- -----------------------------Carousel------------------------------------ -->
         <div id="container">
             <div id="Carousel">
