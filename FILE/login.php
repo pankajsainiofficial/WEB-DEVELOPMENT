210b0c264527a8a991ff9a53bb48b515893de12d
pankaj <pankajsaini19967@gmail.com>
2021-07-07 00:58:14 +0530

    Co-authored-by: parishjainofficial@gmail.com <parishjainofficial@gmail.com> Co-authored-by: sharmavarun1996.vs@gmail.com <sharmavarun1996.vs@gmail.com>

---
+++ FILE/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>LOGIN</title>
+<head>
+   <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="/css/login.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table  >
+        <tr >
+            <td> USER_NAME:</td>
+            <td>PASSWORD:</td>
+            </tr>
+</body>
+</html>
